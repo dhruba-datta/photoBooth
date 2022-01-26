7bc0e4b62fdbd4fc2c47bc04e958169b4c0f3512
dhruba-datta <dhrubadattaanjan@gmail.com>
2022-01-27 02:42:17 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,13 @@ import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-//   useEffect(() => {
-//     const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
 
-//     if (!User) navigate('/login');
-//   }, []);
+    if (!User) navigate('/login');
+  }, []);
 
   return (
     <Routes>
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,7 +3,7 @@ export { default as Feed } from './Feed';
 export { default as PinDetail } from './PinDetail';
 export { default as CreatePin } from './CreatePin';
 export { default as Search } from './Search';
-// export { default as MasonryLayout } from './MasonryLayout';
+export { default as MasonryLayout } from './MasonryLayout';
 export { default as Sidebar } from './Sidebar';
 export { default as Login } from './Login';
 export { default as UserProfile } from './UserProfile';
